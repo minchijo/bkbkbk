94d66a3a2ee6de75a55f6c1c2023deb87da579e7
minchi <87708449+minchijo@users.noreply.github.com>
2024-07-24 22:10:17 +0900

    Update BestsellerVO.java

--- domain/BestsellerVO.java
+++ domain/BestsellerVO.java
@@ -4,14 +4,12 @@ import lombok.Data;
 
 @Data
 public class BestsellerVO {
-		private Integer bestNum;
-	   private Integer cateNum;
-	   private String bestTitle;
-	   private Integer bestIsbn;
-	   private String bestWriter;
-	   private String bestPublisher;
-	   private String bestPubDate;
-	   private String bestfrealname;
-	   private String bestfname;
-	   private String bestPrev;
+    private Integer bestNum;
+    private String cateNum;
+    private String bestTitle;
+    private String bestIsbn;
+    private String bestWriter;
+    private String bestPublisher;
+    private String bestPubDate;
+    private String bestfname;
 }
