9957705849986efea6aa7f91db9d7f83296a9552
minchi <87708449+minchijo@users.noreply.github.com>
2024-07-24 22:08:58 +0900

    Update MainDAO.java

--- dao/MainDAO.java
+++ dao/MainDAO.java
@@ -10,9 +10,16 @@ import com.bookbookbook.domain.ChatroomVO;
 public interface MainDAO {
 
 	//##################################
-	//채팅
+	//채팅방 불러오기
 	public List<ChatroomVO> getChatroom();
+	//빈 방 삭제
+	//public void emptyRoom();
+	//방 생성
 	public void createChatroom(ChatroomVO chat);
+	//방 검색
 	public List<ChatroomVO> searchChatroom(String keyword);
+	//방 정보 가져오기
 	public ChatroomVO getRoomInfo(String roomNum);
+	//방 삭제
+	public void deleteChatroom(String roomNum);
 }
