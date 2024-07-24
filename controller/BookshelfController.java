63f27261cd73b094bb69f92b8728edd6a38ff7ba
minchi <87708449+minchijo@users.noreply.github.com>
2024-07-24 22:07:15 +0900

    Update BookshelfController.java

--- controller/BookshelfController.java
+++ controller/BookshelfController.java
@@ -18,28 +18,32 @@ public class BookshelfController {
 	private BookshelfService bookshelfService;
 
 	/*
-	 * 화면: 나의 서재 전체 탭팬 클릭 시
-	 * method명: getUserData
-	 * 인자:      Model 객체
-	 * 리턴형:     String (페이지 이름)
-	 * 역할:      BookshelfService를 통해 사용자 데이터를 가져와서 모델에 추가하고,
-	 *           "pages/user-myBookshelf" 페이지를 반환하는 함수
+	 * 화면: 				나의 서재 전체, 읽은 책, 읽고 있는 책, 읽고 싶은 책 탭팬 정보 출력
+	 * method명: 	getUserData
+	 * 인자:      			Model 객체
+	 * 리턴형:     		String 
+	 * 역할:      			BookshelfService를 통해 사용자 데이터를 가져와서 모델에 추가하고,
+	 *           				"pages/user-myBookshelf" 페이지를 반환하는 함수
 	 */
 	  @GetMapping("/pages/user-myBookshelf")
 	    public String getUserData(Model model) {
 		  
+		  //전체 탭팬
 	        List<BookshelfVO> getUserData = bookshelfService.getUserData();
 	        System.out.println(">>> Controller에서 받은 데이터: " + getUserData);
 	        model.addAttribute("characterStages", getUserData);
 	        
+	        //읽은 책 탭팬
 	        List<BookshelfVO> getReadBooksDetails = bookshelfService.getReadBooksDetails();
 	        System.out.println(">>> Controller에서 받은 데이터: " + getReadBooksDetails);
 	        model.addAttribute("readBooks", getReadBooksDetails);
 	        
+	        //읽고 있는 책 탭팬
 	        List<BookshelfVO> getReadingBooksDetails = bookshelfService.getReadingBooksDetails();
 	        System.out.println(">>> Controller에서 받은 데이터: " + getReadingBooksDetails);
 	        model.addAttribute("readingBooks", getReadingBooksDetails);
 	        
+	        //읽고 싶은 책 탭팬
 	        List<BookshelfVO> wishlistBooks = bookshelfService.getWishlistBooks();
 	        System.out.println(">>> Controller에서 받은 데이터: " + wishlistBooks);
 	        model.addAttribute("wishlistBooks", wishlistBooks);
