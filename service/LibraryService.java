3b4cbffde86bdc56bfb0b1ac2465d486b42c620a
minchi <87708449+minchijo@users.noreply.github.com>
2024-07-25 11:49:42 +0900

    Update LibraryService.java

--- service/LibraryService.java
+++ service/LibraryService.java
@@ -1,7 +1,22 @@
 package com.bookbookbook.service;
 
+import java.util.HashMap;
+import java.util.List;
+
+import com.bookbookbook.domain.BookVO;
+import com.bookbookbook.domain.CategoryVO;
+import com.bookbookbook.domain.LibraryVO;
+
 public interface LibraryService {
+	//도서관 검색, 책 검색, 책 카테고리 검색
+	
+	//책 카테고리 검색
+	public List<CategoryVO> loadCate();
+	public List<CategoryVO> getCategory(CategoryVO cate);
+	public List<CategoryVO> getCategorySecond(CategoryVO cate);
+	public List<BookVO> getCateBookList(HashMap map);
 
 	
-		//도서관 검색, 책 검색, 책 카테고리 검색
+	//도서 소장 도서관 조회
+	List<LibraryVO> getAllRegions();
 }
