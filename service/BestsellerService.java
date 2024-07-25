97bcade6ee7a7e48683af7a01378e0f0aa274a25
minchi <87708449+minchijo@users.noreply.github.com>
2024-07-25 11:46:27 +0900

    베스트셀러 서비스 추가

---
+++ service/BestsellerService.java
@@ -0,0 +1,6 @@
+
+ package com.bookbookbook.service;
+ 
+ import com.bookbookbook.domain.BestsellerVO; import java.util.List;
+ 
+ public interface BestsellerService { List<BestsellerVO> getPopularBooks(); }
