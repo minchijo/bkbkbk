c8e7fe325c528e57ce569becb9503d52411f2778
minchi <87708449+minchijo@users.noreply.github.com>
2024-07-25 11:53:14 +0900

    Update UserService.java

--- service/UserService.java
+++ service/UserService.java
@@ -1,5 +1,11 @@
 package com.bookbookbook.service;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import com.bookbookbook.domain.CalendarVO;
+import com.bookbookbook.domain.MemoVO;
 import com.bookbookbook.domain.ReportVO;
 import com.bookbookbook.domain.UserVO;
 
@@ -13,4 +19,21 @@ public interface UserService {
 
 	//채팅
 	public void submitReport(ReportVO vo);
+	
+	//#####################################
+		// 나의 달력
+			// 메모 정보 조회
+		List<HashMap<String, Object>> getMemosAtCalendar(String userId);
+		
+			// 출석체크 데이터베이스에 저장
+		boolean  markAttendance(String userId);
+			// 데이터베이스에서 출석 정보 조회
+		List<CalendarVO> getAttendanceList(String userId);
+		
+		//#####################################
+		// 나의 캐릭터
+			// userId로 userLevel에 따른 캐릭터 정보 조회
+		List<HashMap<String, Object>> getCharactersByUserId(String userId);
+			// 캐릭터 상세
+		Map<String, Object> myCharactersDetail(Map<String, Object> params);
 }
